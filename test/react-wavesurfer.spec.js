38c17066bad54970b9b5a300d18f66c2d5dae26e
Martin Spencer <mail@martinspencer.de>
2015-12-29 19:48:53 +0100

    tests working basically

--- test/react-wavesurfer.spec.js
+++ test/react-wavesurfer.spec.js
@@ -2,23 +2,24 @@
 
 jest.dontMock('../src/react-wavesurfer.js');
 
-const Wavesurfer = require('../src/react-wavesurfer');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 
-describe('React Wavesurfer component', () => {
+const WavesurferComponent = require('../src/react-wavesurfer').default;
+global.WaveSurfer = require('../vendor/wavesurfer-bundle.js');
 
+describe('React Wavesurfer component', () => {
 
   it('should run the tests', () => {
     expect(true).toBe(true);
   });
 
   it('should render component', () => {
-    let wavesurfer = TestUtils.renderIntoDocument(<Wavesurfer></Wavesurfer>);
-    //let component = TestUtils.findRenderedComponentWithType(wavesurfer, Wavesurfer);
-    //expect(TestUtils.isElementOfType(component), Wavesurfer);
+    let wavesurfer = TestUtils.renderIntoDocument(<WavesurferComponent />);
+    let component = TestUtils.findRenderedComponentWithType(wavesurfer, WavesurferComponent);
+    expect(TestUtils.isElementOfType(component), WavesurferComponent);
   });
 
 });

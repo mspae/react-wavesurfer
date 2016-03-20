2a8f04b3f5edebbe9164b8ebde20ed974b2030ca
Martin Spencer <mail@martinspencer.de>
2016-03-20 16:32:31 +0100

    removed old commented code for plugins

--- example/index.jsx
+++ example/index.jsx
@@ -1,6 +1,8 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Wavesurfer from 'react-wavesurfer.js';
+
 
 /**
  * Simple example of a React component with a Wavesurfer
@@ -60,79 +62,6 @@ class SimpleExample extends React.Component {
     );
   }
 }
-
-
-/**
- * Example with the timeline plugin
- */
-/*const WaveSurferTimeline = require('wavesurfer.js/plugin/wavesurfer.timeline.js');
-
-class TimelineExample extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  addTimeline(e) {
-    // Init Timeline plugin
-    var timeline = Object.create(WaveSurferTimeline);
-    timeline.init({
-        wavesurfer: e.wavesurfer,
-        container: '#wave-timeline'
-    });
-  }
-  render() {
-    return (
-      <div className='example'>
-        <p>With timeline plugin enabled and a few wavesurfer options set</p>
-          <div id='wave-timeline' />
-          <Wavesurfer
-            audioFile={this.props.audioFile}
-            onReady={this.addTimeline}
-            options={{
-              progressColor: '#906643',
-              waveColor: '#eee',
-              height: 50
-            }}
-          />
-      </div>
-    );
-  }
-}*/
-
-
-/**
- * Example with the timeline plugin
- */
-/*class RegionsExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      regions: [{
-        id: 'One',
-        start: 0,
-        end: 3
-      }, {
-        id: 'Two',
-        start: 4,
-        end: 7
-      }, {
-        id: 'Three',
-        start: 9,
-        end: 13
-      }]
-    };
-  }
-  render() {
-    return (
-      <div className='example'>
-        <p>With regions plugin enabled</p>
-          <Wavesurfer
-            audioFile={this.props.audioFile}
-            regions={this.state.regions}
-          />
-      </div>
-    );
-  }
-}*/
 
 
 class ExampleParent extends React.Component {

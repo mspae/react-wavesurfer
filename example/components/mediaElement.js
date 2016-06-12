740c165205fa6d8f7436fed550cf3bb71f6da067
Martin Spencer <mail@martinspencer.de>
2016-06-12 13:21:15 +0200

    updated mediaElt example

--- example/components/mediaElement.js
+++ example/components/mediaElement.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Wavesurfer from '../../src/react-wavesurfer';
 
-const MediaElementExample = () => (
+class MediaElementExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this._getAudioSource = this._getAudioSource.bind(this);
+  }
+
+  // only after the initial render the audio source element exists,
+  // so force a rerender
+  componentDidMount() {
+    this.forceUpdate();
+  }
+
+  _getAudioSource() {
+    return this.refs.audioSource;
+  }
+
+  // if audio source dom element exists (after first render),
+  // render the wavesurfer component
+  render() {
+    return (
       <div className="example col-xs-12">
         <h3>Media element audio source</h3>
-        <audio src="../resources/demo.wav" id="audioSource" />
-        <Wavesurfer
-          mediaElt="#audioSource"
-        />
+        <audio src="../resources/demo.wav" id="audioSource" ref="audioSource" />
+        {(this._getAudioSource())
+        ? <Wavesurfer mediaElt={this._getAudioSource()} />
+        : null}
+        <Wavesurfer mediaElt="#audioSource" />
       </div>
-);
+    );
+  }
+}
 
 module.exports = MediaElementExample;

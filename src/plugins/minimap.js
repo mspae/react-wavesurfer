879025c613ff8122ac26bc1a37fe3e5fe94def0d
Martin Spencer <mail@martinspencer.de>
2016-09-25 17:31:57 +0200

    better event listener for ready event in minimap plugin

--- src/plugins/minimap.js
+++ src/plugins/minimap.js
@@ -5,16 +5,24 @@ class Minimap extends Component {
 
   componentDidMount() {
     this._map = undefined;
-    this.props.wavesurfer.on('ready', this._init.bind(this));
+
+    // on('ready') returns an event descriptor which is an
+    // object which has the property un, which is the un method
+    // properly bound to this callback, we cache it and can call
+    // it alter to just remove this event listener
+    this._readyListener = this.props.wavesurfer.on('ready', () => {
+      this._init();
+    });
+  }
+
+  componentWillUnmount() {
+    this._readyListener.un();
   }
 
   _init() {
     this._map = Object.create(WaveSurfer.Minimap);
     this._map.init(this.props.wavesurfer, this.props.options);
     this._map.render();
-    console.log(this._map);
-    //this._map.backend.drawer.empty();
-    this._map.drawWave();
   }
 
   render() {
